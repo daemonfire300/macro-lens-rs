56525cc49cb7db7343b4de479ed48071e98802be
Julius Foitzik <info@accountr.eu>
2025-10-30 18:06:15 +0100

    setup nix develop shell

--- flake.nix
+++ flake.nix
@@ -13,15 +13,20 @@
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
       in {
-        devShells.default = with pkgs;
-          mkShell {
-            buildInputs =
-              [ openssl pkg-config eza fd rust-bin.stable.latest.default ];
 
-            shellHook = ''
-              echo "Entering system-specific shell <$SHELL>"
-              exec $SHELL
-            '';
-          };
+        devShells.default = let
+          stable-rust = pkgs.rust-bin.stable.latest;
+          rust-core = stable-rust.default;
+          stable-rust-analyzer = stable-rust.rust-analyzer;
+        in with pkgs;
+        mkShell {
+          buildInputs =
+            [ openssl pkg-config eza fd rust-core stable-rust-analyzer ];
+
+          shellHook = ''
+            echo "Entering system-specific shell <$SHELL>"
+            #exec $SHELL
+          '';
+        };
       });
 }
